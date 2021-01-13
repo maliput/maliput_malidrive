31ffd03d830f2d3eb1918f0094d7873263ca495a
Franco Cipollone <franco.c@ekumenlabs.com>
2021-01-13 12:38:59 -0300

    Parse all the RoadNetworkConfiguration values.

--- maliput_malidrive/src/plugin/road_network.cc
+++ maliput_malidrive/src/plugin/road_network.cc
@@ -1,5 +1,6 @@
-// Copyright 2020 Toyota Research Institute
+// Copyright 2021 Toyota Research Institute
 #include <memory>
+#include <optional>
 
 #include "maliput/plugin/road_network_loader.h"
 #include "maliput_malidrive/builder/road_network_builder.h"
@@ -9,43 +10,69 @@ namespace malidrive {
 namespace plugin {
 namespace {
 
-malidrive::builder::RoadNetworkConfiguration GetPropertiesFromStringMap(
-    const std::map<std::string, std::string>& parameters) {
-  std::string id{"Maliput_malidrive RoadGeometry"};
+// Return a builder::RoadNetworkConfiguration object out of a map of strings.
+// @param parameters  A dictionary of properties to fill in a builder::RoadNetworkConfiguration struct.
+//                    Keys are the names of attributes in builder::RoadNetworkConfiguration.
+// @returns A builder::RoadNetworkConfiguration.
+builder::RoadNetworkConfiguration GetPropertiesFromStringMap(const std::map<std::string, std::string>& parameters) {
   auto it = parameters.find("road_geometry_id");
-  if (it != parameters.end()) {
-    id = it->second;
-  }
-  const maliput::api::RoadGeometryId rg_id{id};
+  const maliput::api::RoadGeometryId rg_id{it != parameters.end() ? it->second : "Maliput_malidrive RoadGeometry"};
 
-  std::string opendrive_file;
   it = parameters.find("opendrive_file");
-  if (it != parameters.end()) {
-    opendrive_file = it->second;
-  }
+  const std::string opendrive_file{it != parameters.end() ? it->second : ""};
 
-  double linear_tolerance{malidrive::constants::kLinearTolerance};
   it = parameters.find("linear_tolerance");
-  if (it != parameters.end()) {
-    linear_tolerance = std::stod(it->second);
-  }
-  double angular_tolerance{malidrive::constants::kAngularTolerance};
+  const double linear_tolerance{it != parameters.end() ? std::stod(it->second) : constants::kLinearTolerance};
+
   it = parameters.find("angular_tolerance");
-  if (it != parameters.end()) {
-    angular_tolerance = std::stod(it->second);
-  }
-  double scale_length{malidrive::constants::kScaleLength};
+  const double angular_tolerance{it != parameters.end() ? std::stod(it->second) : constants::kAngularTolerance};
+
   it = parameters.find("scale_length");
-  if (it != parameters.end()) {
-    scale_length = std::stod(it->second);
-  }
-  // TODO: Parse more parameters.
-  malidrive::InertialToLaneMappingConfig inertial_to_lane(malidrive::constants::kExplorationRadius,
-                                                          malidrive::constants::kNumIterations);
-  return {rg_id, opendrive_file, linear_tolerance, angular_tolerance, scale_length, inertial_to_lane};
+  const double scale_length{it != parameters.end() ? std::stod(it->second) : constants::kScaleLength};
+
+  // TODO(#4): Not being parsed because due to the fact that it is not used in the maliput_malidrive backend.
+  const InertialToLaneMappingConfig inertial_to_lane(constants::kExplorationRadius, constants::kNumIterations);
+
+  it = parameters.find("build_policy");
+  const builder::BuildPolicy build_policy{it != parameters.end() ? builder::BuildPolicy::FromStrToType(it->second)
+                                                                 : builder::BuildPolicy::Type::kSequential};
+
+  it = parameters.find("simplification_policy");
+  const builder::RoadGeometryConfiguration::SimplificationPolicy simplification_policy{
+      it != parameters.end() ? builder::RoadGeometryConfiguration::FromStrToSimplificationPolicy(it->second)
+                             : builder::RoadGeometryConfiguration::SimplificationPolicy::kNone};
+
+  it = parameters.find("tolerance_selection_policy");
+  const builder::RoadGeometryConfiguration::ToleranceSelectionPolicy tolerance_selection_policy{
+      it != parameters.end() ? builder::RoadGeometryConfiguration::FromStrToToleranceSelectionPolicy(it->second)
+                             : builder::RoadGeometryConfiguration::ToleranceSelectionPolicy::kManualSelection};
+
+  it = parameters.find("road_rule_book");
+  const std::optional<std::string> road_rule_book{it != parameters.end() ? std::make_optional(it->second)
+                                                                         : std::nullopt};
+
+  it = parameters.find("traffic_light_book");
+  const std::optional<std::string> traffic_light_book{it != parameters.end() ? std::make_optional(it->second)
+                                                                             : std::nullopt};
+
+  it = parameters.find("phase_ring_book");
+  const std::optional<std::string> phase_ring_book{it != parameters.end() ? std::make_optional(it->second)
+                                                                          : std::nullopt};
+
+  it = parameters.find("intersection_book");
+  const std::optional<std::string> intersection_book{it != parameters.end() ? std::make_optional(it->second)
+                                                                            : std::nullopt};
+
+  return {{rg_id, opendrive_file, linear_tolerance, angular_tolerance, scale_length, inertial_to_lane, build_policy,
+           simplification_policy, tolerance_selection_policy},
+          road_rule_book,
+          traffic_light_book,
+          phase_ring_book,
+          intersection_book};
 }
 
-class RoadNetwork : public maliput::plugin::RoadNetworkLoader {
+// Implementation of a maliput::plugin::RoadNetworkLoader using maliput_malidrive backend.
+class RoadNetworkLoader : public maliput::plugin::RoadNetworkLoader {
  public:
   std::unique_ptr<const maliput::api::RoadNetwork> operator()(
       const std::map<std::string, std::string>& properties) const override {
@@ -55,7 +82,7 @@ class RoadNetwork : public maliput::plugin::RoadNetworkLoader {
 
 }  // namespace
 
-REGISTER_ROAD_NETWORK_LOADER_PLUGIN("maliput_malidrive", RoadNetwork);
+REGISTER_ROAD_NETWORK_LOADER_PLUGIN("maliput_malidrive", RoadNetworkLoader);
 
 }  // namespace plugin
 }  // namespace malidrive
