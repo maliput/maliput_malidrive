af220ec98eb52127a91bda3a7c812c799f43eea0
Franco Cipollone <franco.c@ekumenlabs.com>
2021-06-09 10:48:07 -0300

    Adds tests to OpenRangeValidator

--- maliput_malidrive/test/regression/road_curve/open_range_validator_test.cc
+++ maliput_malidrive/test/regression/road_curve/open_range_validator_test.cc
@@ -9,19 +9,30 @@ namespace test {
 
 GTEST_TEST(OpenRangeValidatorTest, ConstructorValidationTest) {
   const double kMin{0.5};
-  const double kMax{3.};
+  const double kMax{1034.};
   const double kTolerance{1e-3};
   const double kEpsilon{1e-5};
 
+  // no throw
+  EXPECT_NO_THROW({ OpenRangeValidator(kMin, kMax, kTolerance, kEpsilon, OpenRangeValidator::EpsilonUse::kAbsolute); });
+
+  // relative epsilon > tolerance
+  // kEpsilon * (kMax - kMin) > tolerance
+  EXPECT_THROW({ OpenRangeValidator(kMin, kMax, kTolerance, kEpsilon, OpenRangeValidator::EpsilonUse::kRelative); },
+               std::runtime_error);
+
   // min > max
-  EXPECT_THROW({ OpenRangeValidator(kMax, kMin, kTolerance, kEpsilon); }, std::runtime_error);
+  EXPECT_THROW({ OpenRangeValidator(kMax, kMin, kTolerance, kEpsilon, OpenRangeValidator::EpsilonUse::kAbsolute); },
+               std::runtime_error);
 
   // epsilon > tolerance
-  EXPECT_THROW({ OpenRangeValidator(kMin, kMax, kEpsilon, kTolerance); }, std::runtime_error);
+  EXPECT_THROW({ OpenRangeValidator(kMin, kMax, kEpsilon, kTolerance, OpenRangeValidator::EpsilonUse::kAbsolute); },
+               std::runtime_error);
 
   // min + epsilon > max (applies the other way around, it's specially provided
   // in constructor code to validate numerical error).
-  EXPECT_THROW({ OpenRangeValidator(kMin, kMax, 2 * kMax, kMax); }, std::runtime_error);
+  EXPECT_THROW({ OpenRangeValidator(kMin, kMax, 2 * kMax, kMax, OpenRangeValidator::EpsilonUse::kAbsolute); },
+               std::runtime_error);
 }
 
 GTEST_TEST(OpenRangeValidatorTest, RangeTest) {
@@ -30,7 +41,7 @@ GTEST_TEST(OpenRangeValidatorTest, RangeTest) {
   const double kTolerance{1e-3};
   const double kEpsilon{1e-5};
 
-  const OpenRangeValidator dut(kMin, kMax, kTolerance, kEpsilon);
+  const OpenRangeValidator dut(kMin, kMax, kTolerance, kEpsilon, OpenRangeValidator::EpsilonUse::kAbsolute);
   // In the middle of the range.
   {
     const double kS{2.};
@@ -59,6 +70,47 @@ GTEST_TEST(OpenRangeValidatorTest, RangeTest) {
   // Expects throw because of out of bounds.
   {
     const double kS{6.};
+    EXPECT_THROW({ dut(kS); }, std::runtime_error);
+  }
+}
+
+GTEST_TEST(OpenRangeValidatorTest, RelativeEpsilonRangeTest) {
+  const double kMin{0.5};
+  const double kMax{100.5};
+  const double kRange{kMax - kMin};
+  const double kTolerance{1e-3};
+  const double kEpsilon{1e-8};
+  const double kRelativeEpsilon{kEpsilon * kRange};
+
+  const OpenRangeValidator dut(kMin, kMax, kTolerance, kEpsilon, OpenRangeValidator::EpsilonUse::kRelative);
+  // In the middle of the range.
+  {
+    const double kS{2.};
+    EXPECT_DOUBLE_EQ(dut(kS), kS);
+  }
+  // In the maximum of the range.
+  {
+    const double kS{kMax};
+    EXPECT_DOUBLE_EQ(dut(kS), kS - kRelativeEpsilon);
+  }
+  // In the minimum of the range.
+  {
+    const double kS{kMin};
+    EXPECT_DOUBLE_EQ(dut(kS), kS + kRelativeEpsilon);
+  }
+  // Exceeding the maximum but within linear tolerance.
+  {
+    const double kS{kMax + kTolerance / 2.};
+    EXPECT_DOUBLE_EQ(dut(kS), kMax - kRelativeEpsilon);
+  }
+  // Exceeding the minimum but within linear tolerance.
+  {
+    const double kS{kMin - kTolerance / 2.};
+    EXPECT_DOUBLE_EQ(dut(kS), kMin + kRelativeEpsilon);
+  }
+  // Expects throw because of out of bounds.
+  {
+    const double kS{105.};
     EXPECT_THROW({ dut(kS); }, std::runtime_error);
   }
 }
