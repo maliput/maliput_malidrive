3175789207eabb5fc537de5e80f4bebf640b812a
Franco Cipollone <franco.c@ekumenlabs.com>
2022-02-03 17:21:04 -0300

    Creates a RoadRulebookBuilder for new rules.

--- maliput_malidrive/src/maliput_malidrive/builder/road_rulebook_builder_old_rules.cc
+++ maliput_malidrive/src/maliput_malidrive/builder/road_rulebook_builder_old_rules.cc
@@ -6,6 +6,7 @@
 
 #include "maliput_malidrive/builder/builder_tools.h"
 #include "maliput_malidrive/builder/id_providers.h"
+#include "maliput_malidrive/builder/road_rulebook_builder.h"
 
 namespace malidrive {
 namespace builder {
@@ -21,53 +22,32 @@ RoadRuleBookBuilderOldRules::RoadRuleBookBuilderOldRules(
     const maliput::api::RoadGeometry* rg, const maliput::api::rules::RuleRegistry* rule_registry,
     const std::optional<std::string>& road_rulebook_file_path,
     const std::vector<maliput::api::rules::DirectionUsageRule>& direction_usage_rules,
-    const std::vector<maliput::api::rules::SpeedLimitRule>& speed_limit_rules, bool use_loader_with_rule_registry)
+    const std::vector<maliput::api::rules::SpeedLimitRule>& speed_limit_rules)
     : rg_(rg),
       rule_registry_(rule_registry),
       road_rulebook_file_path_(road_rulebook_file_path),
       direction_usage_rules_(direction_usage_rules),
-      speed_limit_rules_(speed_limit_rules),
-      use_loader_with_rule_registry_(use_loader_with_rule_registry) {
+      speed_limit_rules_(speed_limit_rules) {
   MALIDRIVE_THROW_UNLESS(rg_ != nullptr);
   MALIDRIVE_THROW_UNLESS(rule_registry_ != nullptr);
 }
 
 std::unique_ptr<const maliput::api::rules::RoadRulebook> RoadRuleBookBuilderOldRules::operator()() {
   // TODO(francocipollone): Removes the load method that doesn't use the rule registry.
-  maliput::log()->trace("{}{}",
-                        road_rulebook_file_path_.has_value()
-                            ? "RoadRulebook file provided: " + road_rulebook_file_path_.value()
-                            : "No RoadRulebook file provided",
-                        road_rulebook_file_path_.has_value()
-                            ? (use_loader_with_rule_registry_ ? "\n\tUsing RoadRulebook loader with RuleRegistry"
-                                                              : "\n\tUsing RoadRulebook loader without RuleRegistry")
-                            : "");
+  maliput::log()->trace("{}", road_rulebook_file_path_.has_value()
+                                  ? "RoadRulebook file provided: " + road_rulebook_file_path_.value()
+                                  : "No RoadRulebook file provided");
 
-  auto rulebook = !road_rulebook_file_path_.has_value()
-                      ? std::make_unique<maliput::ManualRulebook>()
-                      : use_loader_with_rule_registry_
-                            ? maliput::LoadRoadRulebookFromFile(rg_, road_rulebook_file_path_.value(), *rule_registry_)
-                            : maliput::LoadRoadRulebookFromFile(rg_, road_rulebook_file_path_.value());
+  auto rulebook = road_rulebook_file_path_.has_value()
+                      ? maliput::LoadRoadRulebookFromFile(rg_, road_rulebook_file_path_.value())
+                      : std::make_unique<maliput::ManualRulebook>();
 
   maliput::ManualRulebook* rulebook_ptr = dynamic_cast<maliput::ManualRulebook*>(rulebook.get());
   MALIDRIVE_THROW_UNLESS(rulebook_ptr != nullptr);
 
-  // Discrete Value Rules
-  // @{
-  // Creates vehicle usage and vehicle exclusive rules for the entire RoadGeometry.
-  CreateVehicleRelatedRules(rulebook_ptr);
-  // Creates direction usage rules for the entire RoadGeometry.
-  CreateDirectionUsageRules(rulebook_ptr);
-  // @}
+  // Adds rules based on RuleRegistry.
+  RoadRuleBookBuilder::AddsXODRBasedRulesToRulebook(rg_, rule_registry_, rulebook_ptr);
 
-  // Range Value Rules
-  // @{
-  // Creates speed limit rules for the entire RoadGeometry.
-  CreateSpeedLimitRules(rulebook_ptr);
-  // @}
-
-  // TODO(agalbachicar)   Remove when maliput::api::rules::SpeedLimitRules
-  //                      are deprecated.
   // Add speed limit rules.
   for (const auto& speed_limit_rule : speed_limit_rules_) {
     rulebook_ptr->AddRule(speed_limit_rule);
@@ -79,98 +59,6 @@ std::unique_ptr<const maliput::api::rules::RoadRulebook> RoadRuleBookBuilderOldR
   }
 
   return rulebook;
-}
-
-LaneSRoute RoadRuleBookBuilderOldRules::CreateLaneSRouteFor(const Lane* lane) const {
-  MALIDRIVE_THROW_UNLESS(lane != nullptr);
-  return LaneSRoute({LaneSRange(lane->id(), SRange(0., lane->length()))});
-}
-
-void RoadRuleBookBuilderOldRules::CreateVehicleRelatedRules(maliput::ManualRulebook* rulebook) const {
-  MALIDRIVE_DEMAND(rulebook != nullptr);
-  const int severity{Rule::State::kStrict};
-
-  for (const auto& lane_id_lane : rg_->ById().GetLanes()) {
-    const Lane* lane = dynamic_cast<const Lane*>(lane_id_lane.second);
-    MALIDRIVE_THROW_UNLESS(lane != nullptr);
-
-    const std::pair<std::string, std::optional<std::string>> vehicle_rule_values =
-        VehicleUsageAndExclusiveRuleStateValues(lane);
-
-    const LaneSRoute lane_s_route = CreateLaneSRouteFor(lane);
-    Rule::RelatedRules related_rules;
-    if (vehicle_rule_values.second.has_value()) {
-      const Rule::Id rule_id(GetRuleIdFrom(rules::VehicleExclusiveRuleTypeId(), lane->id()));
-      rulebook->AddRule(rule_registry_->BuildDiscreteValueRule(
-          rule_id, rules::VehicleExclusiveRuleTypeId(), lane_s_route,
-          {DiscreteValueRule::DiscreteValue{
-              severity, {} /* related rules */, {} /* related_unique_ids */, vehicle_rule_values.second.value()}}));
-
-      MALIDRIVE_VALIDATE(
-          related_rules.emplace(rules::VehicleExclusiveRuleTypeId().string(), std::vector<Rule::Id>{rule_id}).second,
-          maliput::common::assertion_error,
-          "Failed to fill related rules for vehicle exclusive rule at lane : " + lane_id_lane.first.string());
-    }
-
-    rulebook->AddRule(rule_registry_->BuildDiscreteValueRule(
-        GetRuleIdFrom(rules::VehicleUsageRuleTypeId(), lane->id()), rules::VehicleUsageRuleTypeId(), lane_s_route,
-        {DiscreteValueRule::DiscreteValue{
-            severity, related_rules, {} /* related_unique_ids */, vehicle_rule_values.first}}));
-  }
-}
-
-void RoadRuleBookBuilderOldRules::CreateSpeedLimitRules(maliput::ManualRulebook* rulebook) const {
-  const std::optional<maliput::api::rules::RuleRegistry::QueryResult> speed_limit_query_result =
-      rule_registry_->GetPossibleStatesOfRuleType(maliput::SpeedLimitRuleTypeId());
-  MALIDRIVE_THROW_UNLESS(speed_limit_query_result.has_value());
-  const auto ranges_ptr = std::get_if<std::vector<RangeValueRule::Range>>(&speed_limit_query_result->rule_values);
-  MALIDRIVE_THROW_UNLESS(ranges_ptr != nullptr);
-
-  // Returns an optional bearing a RangeValueRule::Range whose maximum is
-  // `max_speed_limit` when found. Otherwise, std::nullopt is returned.
-  auto range_from_max_speed_limit = [ranges = *(ranges_ptr)](double max_speed_limit) {
-    std::optional<RangeValueRule::Range> result;
-    const auto range_it =
-        std::find_if(ranges.begin(), ranges.end(),
-                     [max_speed_limit](const RangeValueRule::Range& range) { return range.max == max_speed_limit; });
-    if (range_it != ranges.end()) {
-      result = *range_it;
-    }
-    return result;
-  };
-
-  for (const auto& lane_id_lane : rg_->ById().GetLanes()) {
-    const Lane* lane = dynamic_cast<const Lane*>(lane_id_lane.second);
-    MALIDRIVE_THROW_UNLESS(lane != nullptr);
-
-    const auto max_speed_limits = GetMaxSpeedLimitFor(lane);
-    UniqueIntegerProvider speed_limit_indexer(0);
-    for (const auto& speed_limit : max_speed_limits) {
-      const Rule::Id rule_id = GetRuleIdFrom(maliput::SpeedLimitRuleTypeId(), lane->id(), speed_limit_indexer.new_id());
-      const std::optional<RangeValueRule::Range> range = range_from_max_speed_limit(speed_limit.max);
-      const LaneSRoute lane_s_route({LaneSRange(
-          lane->id(), SRange(lane->LaneSFromTrackS(speed_limit.s_start), lane->LaneSFromTrackS(speed_limit.s_end)))});
-      MALIDRIVE_VALIDATE(range.has_value(), maliput::common::assertion_error,
-                         "Failed to obtain speed limit rule range for lane : " + lane_id_lane.first.string());
-      rulebook->AddRule(
-          rule_registry_->BuildRangeValueRule(rule_id, maliput::SpeedLimitRuleTypeId(), lane_s_route, {*range}));
-    }
-  }
-}
-
-void RoadRuleBookBuilderOldRules::CreateDirectionUsageRules(maliput::ManualRulebook* rulebook) const {
-  MALIDRIVE_THROW_UNLESS(rulebook != nullptr);
-  const int severity{Rule::State::kStrict};
-  for (const auto& lane_id_lane : rg_->ById().GetLanes()) {
-    const Rule::RelatedRules empty_related_rules;
-    const Rule::RelatedUniqueIds empty_related_unique_ids;
-    const Lane* lane = dynamic_cast<const Lane*>(lane_id_lane.second);
-    rulebook->AddRule(rule_registry_->BuildDiscreteValueRule(
-        GetRuleIdFrom(maliput::DirectionUsageRuleTypeId(), lane->id()), maliput::DirectionUsageRuleTypeId(),
-        CreateLaneSRouteFor(lane),
-        {DiscreteValueRule::DiscreteValue{severity, empty_related_rules, empty_related_unique_ids,
-                                          GetDirectionUsageRuleStateType(lane)}}));
-  }
 }
 
 }  // namespace builder
--- maliput_malidrive/src/maliput_malidrive/builder/road_rulebook_builder_old_rules.h
+++ maliput_malidrive/src/maliput_malidrive/builder/road_rulebook_builder_old_rules.h
@@ -18,40 +18,27 @@ namespace builder {
 
 /// Functor to build a RoadRulebook.
 ///
-/// At the moment there are two APIs for the rules in maliput.
-/// - The deprecated rules API, which is composed of:
+/// The deprecated rule API is composed of.
 ///   - maliput::api::rules::SpeedLimitRule
 ///   - maliput::api::rules::RightOfWayRule
 ///   - maliput::api::rules::DirectionUsageRule
-/// - And the new rules API which is composed of:
-///   - maliput::api::rules::Rules depending on the type of value they hold:
-///     - maliput::api::rules::DiscreteValueRule
-///     - maliput::api::rules::RangeValueRule
-///   - maliput::api::rules::RuleRegistry: it is used to register all the rule type that are allowed.
 ///
-/// The building procedure changes depending on the various combinations of input parameters. See the detailed
-/// explanation below.
-///
-///  - When no `road_rulebook_file_path` is provided: rules are created out of XODR decription.
-///  - When `road_rulebook_file_path` is provided and `rule_registry` is nullptr: rules with the old API are built out of the
-///    YAML file and created maliput::api::rules::RightOfWayRules are used to create their analogous version
+///  Rules with the old API are built out of the YAML file and created maliput::api::rules::RightOfWayRules are used to
+///  create their analogous version
 ///    as maliput::api::rules::DiscreteValueRules of type "Right-Of-Way-Rule" and "Vehicle-In-Stop-Behavior-Rule".
-///    @see [RoadRulebook loader without rule registry](https://github.com/ToyotaResearchInstitute/maliput/blob/ab4bff490702c31abd2d0d9ff87383f6f00c45c8/maliput/src/base/road_rulebook_loader.cc#L351)
-///    provided by maliput.
-///  - When `road_rulebook_file_path` is provided and `rule_registry` is not nullptr: rules with the new API are created.
-///    Old maliput::api::rules::RightOfWayRule rules won't be filled because there is no matching type in the
-///    `rule_regitsry` for them. See [RoadRulebook loader with rule registry](https://github.com/ToyotaResearchInstitute/maliput/blob/ab4bff490702c31abd2d0d9ff87383f6f00c45c8/maliput/src/base/road_rulebook_loader_using_rule_registry.cc#L206)
+///    @see [RoadRulebook loader without rule
+///    registry](https://github.com/ToyotaResearchInstitute/maliput/blob/ab4bff490702c31abd2d0d9ff87383f6f00c45c8/maliput/src/base/road_rulebook_loader.cc)
 ///    provided by maliput.
 ///
 /// The following rules are created regardless the values of `road_rulebook_file_path` and `rule_registry`:
-///  - Speed limits: maliput::api::rules::SpeedLimitRule and maliput::api::rules::RangeValueRule whose type 
+///  - Speed limits: maliput::api::rules::SpeedLimitRule and maliput::api::rules::RangeValueRule whose type
 ///    is "Speed-Limit-Rule" are created.
 ///  - Direction usage: maliput::api::rules::DirectionUsageRule and maliput::api::rules::DiscretValueRule whose type
 ///    is "Speed-Limit-Rule" are created.
 ///  - Vehicle exclusive: maliput::api::rules::DiscretValueRule whose type is "Vehicle-Exclusive-Rule" are created.
 ///  - Vehicle usage: maliput::api::rules::DiscretValueRule whose type is "Vehicle-Usage-Rule" are created.
 ///
-/// TODO(ToyotaResearchInstitute/maliput#108): remove deprecated rules API.
+/// TODO(ToyotaResearchInstitute/maliput#108): remove RoadRulebook builder functor
 class RoadRuleBookBuilderOldRules {
  public:
   MALIDRIVE_NO_COPY_NO_MOVE_NO_ASSIGN(RoadRuleBookBuilderOldRules)
@@ -64,13 +51,12 @@ class RoadRuleBookBuilderOldRules {
   /// @param road_rulebook_file_path to the yaml file to load the RoadRulebook.
   /// @param direction_usage_rules is a vector of DirectionUsageRules.
   /// @param speed_limit_rules is a vector of SpeedLimitRules.
-  /// @param use_loader_with_rule_registry is a boolean to determine which maliput RoadRulebook loader to use.
   /// @throw maliput::assertion_error When `rg` or `rule_registry` are nullptr.
-  RoadRuleBookBuilderOldRules(const maliput::api::RoadGeometry* rg, const maliput::api::rules::RuleRegistry* rule_registry,
-                      const std::optional<std::string>& road_rulebook_file_path,
-                      const std::vector<maliput::api::rules::DirectionUsageRule>& direction_usage_rules,
-                      const std::vector<maliput::api::rules::SpeedLimitRule>& speed_limit_rules,
-                      bool use_loader_with_rule_registry);
+  RoadRuleBookBuilderOldRules(const maliput::api::RoadGeometry* rg,
+                              const maliput::api::rules::RuleRegistry* rule_registry,
+                              const std::optional<std::string>& road_rulebook_file_path,
+                              const std::vector<maliput::api::rules::DirectionUsageRule>& direction_usage_rules,
+                              const std::vector<maliput::api::rules::SpeedLimitRule>& speed_limit_rules);
 
   /// Builds a ManualRulebook.
   ///
@@ -79,59 +65,11 @@ class RoadRuleBookBuilderOldRules {
   std::unique_ptr<const maliput::api::rules::RoadRulebook> operator()();
 
  private:
-  // @returns A LaneSRoute that covers `lane.`
-  // @throws maliput::common::assertion_error When `lane` is nullptr.
-  maliput::api::LaneSRoute CreateLaneSRouteFor(const Lane* lane) const;
-
-  // Creates vehicle usage and vehicle exclusive rules for all Lanes in the
-  // RoadGeometry.
-  // Rule values are created based on the XODR Lane Type.
-  //
-  // @param rulebook The pointer to the RoadRulebook based to add the rules.
-  //        It must not be nullptr.
-  void CreateVehicleRelatedRules(maliput::ManualRulebook* rulebook) const;
-
-  // Creates speed limit rules for all Lanes in the RoadGeometry.
-  // Rule values have been previously registered in the
-  // maliput::api::rules::RuleRegistry, which was provided at time of
-  // construction.
-  //
-  // Speed limit rules naming convention:
-  // Rule Id: "<RuleType> + '/' + <LaneID> + '_' + <index>".
-  // Where 'index' is a number that increments in order to differentiate between speed
-  // rules limit within a Lane.
-  //
-  // @param rulebook The RoadRulebook to which to add the rules.
-  //        It must not be nullptr.
-  //
-  // @throws maliput::common::assertion_error When `rulebook` is nullptr.
-  // @throws maliput::common::assertion_error When
-  // maliput::api::rules::RuleRegistry has no type equal to
-  // maliput::SpeedLimitRuleTypeId().
-  // @throws maliput::common::assertion_error When a Lane in the RoadGeometry
-  // has a max speed limit that does not match any range in
-  // maliput::SpeedLimitRuleTypeId() rule type.
-  void CreateSpeedLimitRules(maliput::ManualRulebook* rulebook) const;
-
-  // Creates direction usage rules for the entire RoadGeometry.
-  // Direction Usage Rule Type must have been previously registered in the
-  // maliput::api::rules::RuleRegistry, which was provided at time of
-  // construction.
-  //
-  // @param rulebook The RoadRulebook to add the rules.
-  //        It must not be nullptr.
-  //
-  // @throws maliput::common::assertion_error When `rulebook` is nullptr.
-  void CreateDirectionUsageRules(maliput::ManualRulebook* rulebook) const;
-
   const maliput::api::RoadGeometry* rg_{};
   const maliput::api::rules::RuleRegistry* rule_registry_{};
   const std::optional<std::string> road_rulebook_file_path_{};
   std::vector<maliput::api::rules::DirectionUsageRule> direction_usage_rules_;
   std::vector<maliput::api::rules::SpeedLimitRule> speed_limit_rules_;
-  std::function<std::pair<std::string, std::optional<std::string>>(const Lane*)>
-      extract_vehicle_usage_and_vehicle_exclusive_from_lane_;
-  bool use_loader_with_rule_registry_{true};
 };
 
 }  // namespace builder
