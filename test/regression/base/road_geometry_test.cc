2c9271dd2e87a88ed72af06c3fc2b393be7d3835
Santiago Lopez <santiago.lopez@ekumenlabs.com>
2025-07-29 17:03:00 -0300

    Implement GeoReferenceInfo API (#330)
    * Implement geo reference API.
    
    Signed-off-by: Santiago Lopez <santiago.lopez@ekumenlabs.com>
    
    * Make parametric tests.
    
    Signed-off-by: Santiago Lopez <santiago.lopez@ekumenlabs.com>
    
    ---------
    
    Signed-off-by: Santiago Lopez <santiago.lopez@ekumenlabs.com>

--- test/regression/base/road_geometry_test.cc
+++ test/regression/base/road_geometry_test.cc
@@ -1299,6 +1299,36 @@ TEST_P(RoadGeometryDoBackendCustomCommand, DoBackendCustomCommand) {
 INSTANTIATE_TEST_CASE_P(RoadGeometryDoBackendCustomCommandGroup, RoadGeometryDoBackendCustomCommand,
                         ::testing::ValuesIn(InstanciateCommandsInputOutputsParameters()));
 
+// Holds the input and expected output for the DoBackendCustomCommand test.
+struct GeoReferenceInfoParams {
+  std::string xodr_file;
+  std::string expected_output;
+};
+
+std::vector<GeoReferenceInfoParams> InstanciateGeoReferenceParameters() {
+  return {{"Town01.xodr", "+lat_0=4.9000000000000000e+1 +lon_0=8.0000000000000000e+0"}, {"ArcLane.xodr", ""}};
+}
+
+class RoadGeometryGeoReferenceInfo : public ::testing::TestWithParam<GeoReferenceInfoParams> {
+ protected:
+  void SetUp() override { road_geometry_configuration_.id = maliput::api::RoadGeometryId("GeoReferenceInfo"); }
+  builder::RoadGeometryConfiguration road_geometry_configuration_{};
+  std::unique_ptr<maliput::api::RoadNetwork> road_network_{nullptr};
+};
+
+TEST_P(RoadGeometryGeoReferenceInfo, DoGeoReferenceInfo) {
+  const auto param = GetParam();
+  road_geometry_configuration_.opendrive_file = utility::FindResourceInPath(param.xodr_file, kMalidriveResourceFolder);
+  road_network_ =
+      ::malidrive::loader::Load<::malidrive::builder::RoadNetworkBuilder>(road_geometry_configuration_.ToStringMap());
+  auto rg = dynamic_cast<const RoadGeometry*>(road_network_->road_geometry());
+  const std::string result = rg->GeoReferenceInfo();
+  EXPECT_EQ(param.expected_output, result);
+}
+
+INSTANTIATE_TEST_CASE_P(RoadGeometryGeoReferenceInfoGroup, RoadGeometryGeoReferenceInfo,
+                        ::testing::ValuesIn(InstanciateGeoReferenceParameters()));
+
 }  // namespace
 }  // namespace tests
 }  // namespace malidrive
