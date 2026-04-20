894a2ca35604881f0227f0b9076624f6a5ed4bd1
Santiago Lopez <santiago.lopez@ekumenlabs.com>
2026-04-20 16:16:51 -0300

    Refactor traffic sign mapper while adding new sign type no_overtaking. (#441)
    * Refactor traffic sign mapper while adding new sign type no_overtaking.
    
    Signed-off-by: Santiago Lopez <santiago.lopez@ekumenlabs.com>
    
    ---------
    
    Signed-off-by: Santiago Lopez <santiago.lopez@ekumenlabs.com>

---
+++ src/maliput_malidrive/builder/traffic_sign_type_mapper.cc
@@ -0,0 +1,63 @@
+// BSD 3-Clause License
+//
+// Copyright (c) 2026, Woven by Toyota. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#include "maliput_malidrive/builder/traffic_sign_type_mapper.h"
+
+#include <algorithm>
+#include <unordered_map>
+
+#include <maliput/common/maliput_hash.h>
+
+namespace malidrive {
+namespace builder {
+
+maliput::api::rules::TrafficSignType MapSignTypeString(const std::string& sign_type_str) {
+  using T = maliput::api::rules::TrafficSignType;
+  static const std::unordered_map<std::string, T, maliput::common::DefaultHash> kMapper{
+      {"stop", T::kStop},
+      {"yield", T::kYield},
+      {"speed_limit", T::kSpeedLimit},
+      {"no_entry", T::kNoEntry},
+      {"one_way", T::kOneWay},
+      {"pedestrian_crossing", T::kPedestrianCrossing},
+      {"no_left_turn", T::kNoLeftTurn},
+      {"no_right_turn", T::kNoRightTurn},
+      {"no_u_turn", T::kNoUTurn},
+      {"school_zone", T::kSchoolZone},
+      {"construction", T::kConstruction},
+      {"railroad_crossing", T::kRailroadCrossing},
+      {"no_overtaking", T::kNoOvertaking},
+  };
+  std::string lower = sign_type_str;
+  std::transform(lower.begin(), lower.end(), lower.begin(), [](unsigned char c) { return std::tolower(c); });
+  const auto it = kMapper.find(lower);
+  return it != kMapper.end() ? it->second : T::kUnknown;
+}
+
+}  // namespace builder
+}  // namespace malidrive
